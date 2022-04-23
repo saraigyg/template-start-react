e59e3f308244a13603904b5f0d78201b8a3ee887
sarai gonzalez <saraiglezyglez@gmail.com>
2022-04-23 14:46:46 -0500

    save and edit posts functions

--- src/App.js
+++ src/App.js
@@ -10,17 +10,30 @@ function App() {
 
   const [isVisible, setIsVisible] = useState(false);
   const [allPosts, setAllPosts] = useState(Posts());
+  const [postId, setPostId] = useState();
 
   const onPress = () => {
     setIsVisible(!isVisible);
   };
 
   const handleOnSave = (post) => {
-    const copyOfPosts = Array.from(allPosts);
-    const newAllPosts = [...copyOfPosts, post];
-    setAllPosts(newAllPosts); 
-    onPress();
+    if(postId) {
+      const copyOfPosts = Array.from(allPosts);
+      const newAllPosts = copyOfPosts.filter((post, index) => index !== postId)//[...copyOfPosts, post];
+      setAllPosts([...newAllPosts, post]); 
+      setPostId();
+    }
+    else {
+      setAllPosts([...allPosts, post]); 
+    }
+    
+    onPress(); // setIsVisible false
   };
+
+  const handleOnEdit = (postId) => {
+    setIsVisible(true); //onPress(true)
+    setPostId(postId);
+  }
 
   return (
     <div className="App">
@@ -29,24 +42,22 @@ function App() {
       {
         isVisible ?
         <CreatePost 
+        postToUpdate={allPosts[postId]}
         onPress={() => onPress()}
         onSave = {handleOnSave}
         />
         :
         <><FeaturedPost updatedAt={"April 20, 2022"}
-            height={500}
-            width={500}
+            height={300}
+            width={300}
             title={"The Internet of Medical Things: The Healthcare Revolution"}
             content={"Since the pandemic started, there has been a rapid effort in different research communities to exploit a wide variety of technologies to combat this worldwide threat, and IoT technology is one of the pioneers in this area."}
             image={"http://speechsynergy.com/wp-content/uploads/2015/08/adult_therapy_final_800w.jpg"}
             blogLink={"https://google.com/"} 
           />  
-          <PostList posts={Posts()} />
+          <PostList posts={allPosts} onEdit={handleOnEdit}/>
         </>
       }
-
-     
-      
     </div>
   );
 }
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,16 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/forms.css";
 
-const CreatePost = ({onPress, toSave}) => {
+const CreatePost = ({onPress, onSave, postToUpdate}) => {
 
     const newPost = {
         title: "",
         body: "",
         imageUrl: "",
-        updatedAt:""
+        updatedAt:(new Date()).toISOString()
     }
 
-    const [newPostState, setNewPostState] = useState(newPost);
+   /*useEffect(()=>{
+        setNewPostState(newPost);
+    }); //[]);*/
+
+    const [newPostState, setNewPostState] = useState(postToUpdate || newPost);
+    const handleOnChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setNewPostState({...newPostState, [name] : value});
+    }
 
     return (
         <div className="container">
@@ -21,6 +30,8 @@ const CreatePost = ({onPress, toSave}) => {
                     type = "text"
                     name = "title"
                     placeholder= "Add a title"
+                    value = {newPostState.title}
+                    onChange={handleOnChange}
                     />
                 </div>
                 <div className="input-field">
@@ -29,13 +40,17 @@ const CreatePost = ({onPress, toSave}) => {
                     type = "text"
                     name = "body"
                     placeholder="Add a body to the post"
+                    value = {newPostState.body}
+                    onChange={handleOnChange}
                     />
                 </div>
                 <div className="input-field">
                     <label>Image</label>
                     <input
-                    type = "file"
-                    name = "image"
+                    type = "text"
+                    name = "imageUrl"
+                    value = {newPostState.imageUrl}
+                    onChange={handleOnChange}
                     />
                 </div>
                 <div className="buttons-container">
@@ -46,7 +61,7 @@ const CreatePost = ({onPress, toSave}) => {
                     </button>
                     <button
                     type="button"
-                    onClick={() => toSave(newPostState)}
+                    onClick={() => onSave(newPostState)}
                     >Save
                     </button>
                 </div>   
